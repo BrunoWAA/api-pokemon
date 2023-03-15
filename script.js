34a46d2a0499ae42df92e28cff43b33eb618be73
mobifilho <83096494+BrunoWAA@users.noreply.github.com>
2023-03-14 22:06:21 -0300

    Commit

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 window.addEventListener("load", function()
     {
+        document.getElementById('container').style.display="none"
         document.getElementById("buscar").addEventListener('click', function ()
         {
             botaoPesquisa()
@@ -10,6 +11,9 @@ window.addEventListener("load", function()
 async function botaoPesquisa() 
 {
     let id = document.getElementById("idPokemon").value
+    if((id >0 && id <1000))
+    {
+    
     const personagem = await buscarPokemon(id)
 
     console.log(personagem)
@@ -17,12 +21,17 @@ async function botaoPesquisa()
     console.log(personagem.types[0].type.name)
     console.log(personagem.types[0].type.url)
 
-    document.getElementById('nomePokemon').innerHTML = ("# "+ id + "- " + personagem.name)
+    document.getElementById('nomePokemon').innerHTML = ("#"+ id + " - " + personagem.name)
     document.getElementById("imagemPokemon").src = (personagem.sprites.front_default)
 
 
     document.getElementById('typePokemon').innerHTML = (personagem.types[0].type.name)
     document.getElementById('type2Pokemon').innerHTML = (personagem.types[1].type.name)
+    document.getElementById('container').style.display="block"
+    }
+    else{
+        document.getElementById('container').style.display="none"
+    }
 }
 
 async function buscarPokemon(id){
